b8c3c60fe454c6c653ed037606c08bd48e7323d2
nicmlu <NicoleMLucien@gmail.com>
2020-01-12 11:10:04 -0500

    saves an instance of the class with the correct id

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,4 +8,15 @@ class Pokemon
         @type = type
         @db = db
     end 
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?);
+        SQL
+
+        db.execute(sql, name, type)
+
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end 
 end
